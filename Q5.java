e999e596a46c71ff0dce33512d30fb21f36d1c87
Vocif3r <felixlgrs.dev@gmail.com>
2025-10-08 16:05:17 +0200

    Q5

---
+++ Q5.java
@@ -0,0 +1,18 @@
+public class Q5
+{
+	public static void main(String[] args)
+	{
+		int n = Integer.parseInt(args[0]);
+		String s = "";
+		for (int i = 1; i <= n; i++)
+		{
+			s = "";
+			for (int j = 1; j <= i; j++)
+			{
+				s += " ";
+				s += j;
+			}
+			System.out.println(s);
+		}
+	}
+}
